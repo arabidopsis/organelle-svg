b0dba655ee16fa0b049d3ece476bb682855274bc
Ian Castleden <ian.castleden@gmail.com>
2026-08-07 01:09:21 +0800

    ruff

--- organelle_svg/api.py
+++ organelle_svg/api.py
@@ -1,25 +1,20 @@
 from __future__ import annotations
 
 __all__ = [
-    "StackedDraw",
     "BaseDraw",
-    "PairsDraw",
-    "OGDraw",
     "DepthDraw",
-    "NormalDraw",
+    "DrawStyle",
     "NCBINormalDraw",
+    "NormalDraw",
+    "OGDraw",
+    "PairsDraw",
+    "StackedDraw",
     "savesvg",
     "tostring",
-    "DrawStyle",
 ]
 
-from .normal_svg import NCBINormalDraw
-from .normal_svg import NormalDraw
-from .ogdraw_svg import BaseDraw
-from .ogdraw_svg import DepthDraw
-from .ogdraw_svg import DrawStyle
-from .ogdraw_svg import OGDraw
+from .normal_svg import NCBINormalDraw, NormalDraw
+from .ogdraw_svg import BaseDraw, DepthDraw, DrawStyle, OGDraw
 from .pairs_svg import PairsDraw
 from .stacked_svg import StackedDraw
-from .svg import savesvg
-from .svg import tostring
+from .svg import savesvg, tostring
